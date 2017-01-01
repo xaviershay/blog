4feeb47512e0d5f41eac86861a15ef5363e1d363
Xavier Shay <xavier@squareup.com>
2017-01-01 11:24:50 +1100

    Upgrade jekyll, remove unused things.

--- Gemfile
+++ Gemfile
@@ -2,6 +2,3 @@ source 'https://rubygems.org'
 
 gem "s3_website"
 gem 'jekyll'
-gem 'jekyll-minibundle'
-gem 'coderay'
-gem 'rake'
--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -17,31 +17,6 @@ $(document).ready(function() {
 
 });
 
-/*
-$(document).ready(function() {
-    var vpH = $(window).height();
-    var vH = vpH - 350;
-    $('.overlay').css("height", vH);
-    $('.featured-image').css("height", vH);
-});
-*/
-
-
-$(function(){
-  $('<img>').attr('src',function(){
-      var imgUrl = $('div.featured-image').css('background-image');
-      if (!imgUrl) {
-        return;
-      }
-      imgUrl = imgUrl.substring(4, imgUrl.length-1);
-      return imgUrl;
-  }).load(function(){
-    $('img.loading').fadeOut(500);
-    $('div.overlay').fadeTo("slow", 0.4);
-    $('div.overlay .credit').fadeTo("slow", 1.0);
-  });
-});
-
 $(function(){
     $('.post-list li').each(function(i){
         var t = $(this);
